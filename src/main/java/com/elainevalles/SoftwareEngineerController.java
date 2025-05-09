c6d469960a7996668d61968ae3351091bbf743f7
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-09 13:01:54 +0200

    config Controller

---
+++ src/main/java/com/elainevalles/SoftwareEngineerController.java
@@ -0,0 +1,33 @@
+package com.elainevalles;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getSoftwareEngineers() {
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "Kyle",
+                        "js, node, react, tailwind, css"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Anne",
+                        "java, spring, springboot"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Thomas",
+                        "java, javaScript, python"
+                )
+        );
+    }
+}
